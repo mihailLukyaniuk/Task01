978d8ada1a34b057b4ad12a5a39ae4f8d7d8e7de
mihailLukyaniuk <43263508+mihailLukyaniuk@users.noreply.github.com>
2019-03-04 23:00:42 +0300

    Update Distance.java

--- Distance.java
+++ Distance.java
@@ -3,16 +3,17 @@
  * to meters and kilometers, respectively.
  */
 public class Distance {
+	
 	final private long KOEFF_METER = 100;
 	final private long KOEFF_KILOMETER =  KOEFF_METER * 10;
 	
 	public long convertToMeter(long distance) {
-		long meter = distance/KOEFF_METER;
+		long meter = distance / KOEFF_METER;
 		return meter;
 	}
 		
 	public long convertToKilometer(long distance) {
-		long kilometer = distance/(KOEFF_KILOMETER);
+		long kilometer = distance / KOEFF_KILOMETER;
 		return kilometer;
 	}
 	
